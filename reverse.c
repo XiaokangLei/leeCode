045400c2c53021e597e7cf60230ab35d92d7fbc5
XiaokangLei <13281183@bjtu.edu.cn>
2019-08-05 19:15:01 +0800

    整数反转

---
+++ reverse.c
@@ -0,0 +1,16 @@
+/*
+    给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ */
+int reverse(int x){
+    long result = 0;
+    while(x){
+        int tmp = x % 10;
+        x /= 10;
+        result = result * 10 + tmp;
+    }
+    if(result > INT_MAX || result <INT_MIN){
+        result = 0;
+    }
+    return (int)result;
+}
+// result： pass,8ms,6.8MB
