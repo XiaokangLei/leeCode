34911e4095aaf4a1015272cbbf070b7bd34ccea2
XiaokangLei <13281183@bjtu.edu.cn>
2019-08-05 19:17:04 +0800

    两数之和

--- twoSum.c
+++ twoSum.c
@@ -1,4 +1,5 @@
 /**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
